f41dfe5c6095bbc602cd58683ca3d6ebfd3c2e1f
Weizun Tang <tangxingbang@gmail.com>
2018-02-19 17:02:19 -0500

    Phone_number variable added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   #protect_from_forgery with: :null_session
 
   #include DeviseTokenAuth::Concerns::ResourceFinder
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number])
+    end
 
 end
