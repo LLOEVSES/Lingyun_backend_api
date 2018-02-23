742dc03b543d40a6da80e74b6a7ddb1b3aebeeab
Weizun Tang <tangxingbang@gmail.com>
2018-02-23 16:30:54 -0500

    d

--- app/controllers/v1/customer_events_controller.rb
+++ app/controllers/v1/customer_events_controller.rb
@@ -13,15 +13,15 @@ class V1::CustomerEventsController < ApplicationController
 
     #customer create new event
     def create
-        event = Event.new(event_params)
-        event.customer_id = current_customer.id
-        #the first worker is empty worker means: searching worker
-        event.worker_id = 2
-        if event.save
-          render json: event
-        else
-          render json: event.errors.messages
-        end
+        # event = Event.new(event_params)
+        # event.customer_id = current_customer.id
+        # #the first worker is empty worker means: searching worker
+        # event.worker_id = 2
+        # if event.save
+        #   render json: event
+        # else
+        #   render json: event.errors.messages
+        # end
         render json: "200"
     end
 
