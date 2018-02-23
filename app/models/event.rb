18295c6e21c12e9687243be8e817606b617695c1
Weizun Tang <tangxingbang@gmail.com>
2018-02-23 16:35:11 -0500

    p

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,7 @@ class Event < ApplicationRecord
     belongs_to :customer
     belongs_to :worker
 
-    validates :address
-    validates :detail
+
 
     enum status: [:searching, :accepted, :processing, :finished, :cancelled]
     enum event_type: [:water, :earth, :fire, :wind]
