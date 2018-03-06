c1cada0665e451ab67dcfc236edc599adfbda8c8
Weizun Tang <tangxingbang@gmail.com>
2018-03-05 21:02:48 -0500

    c

--- config/application.rb
+++ config/application.rb
@@ -11,16 +11,18 @@ module LingyunServerV2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    
+
     config.middleware.use Rack::Cors do
       allow do
         origins '*'
         resource '*',
           :headers => :any,
-          :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+          :expose  => ['Access-Token', 'Expiry', 'Token-Type', 'Uid', 'Client'],
           :methods => [:get, :post, :options, :delete, :put]
       end
     end
+
+    #config.time_zone = 'Beijing'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
