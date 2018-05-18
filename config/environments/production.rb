bc79400da56ea2d475199c9af94407ec89e8a4d8
Weizun Tang <tangxingbang@gmail.com>
2018-05-18 11:10:17 +0800

    g

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,18 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'lingyun-web-api.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => "plain",
+    :user_name => "tangweizun@gmail.com",
+    :password => "TWZ95Tt09",
+    :enable_starttls_auto => true
+
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
